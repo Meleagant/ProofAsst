a34dddd7523a02e6dc75e2b5d7fe062de9433b7b
giet <giet@clipper.ens.fr>
2018-10-30 23:53:28 +0100

    continuation sur le travail et des TODOs dans le README

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # ProofAsst
+
+TODO : 
+[] : refaire le `in_bool` pour quèil ne marche que sur les entiers !
+[] : finir la preuve de terminaison : comment exprimer que les séquents
+sont obtenus par un appel de step ?
+[] : finir la partie sur la soundness !
+[] : mettre des commentaires ! 
--- projet.v
+++ projet.v
@@ -1,4 +1,4 @@
-(* Ceci est le projet du cours d'assistants de preuves *)
+(* Ceci est le projet du cours d'assistants de preuves du MPRI 2-7-2 *)
 (* Josselin GIET M2 DI/ENS *)
 
 
@@ -49,6 +49,16 @@ Ltac tauto1 :=
             |
             clear H; tauto1]
 
+    (* ===/  Rules for \/  \=== *)
+    (* ----------------------- *)
+    (* Rule : [\/ - I] *)
+    | |- ?A \/ ?B =>
+        idtac "\/-I";
+        (left; tauto1) || (right; tauto1)
+    (* Rule : [\/ - E] *)
+    | H: ?A \/ ?B |- _ =>
+        idtac "\/-E";
+        destruct H; [tauto1 | tauto1]
 
     (* ===/  Rules for /\  \=== *)
     (* ----------------------- *)
@@ -60,17 +70,6 @@ Ltac tauto1 :=
     | H: ?A /\ ?B |- _ =>
         idtac "/\-E";
         destruct H; tauto1
-
-    (* ===/  Rules for \/  \=== *)
-    (* ----------------------- *)
-    (* Rule : [\/ - I] *)
-    | |- ?A \/ ?B =>
-        idtac "\/-I";
-        (left; tauto1) || (right; tauto1)
-    (* Rule : [\/ - E] *)
-    | H: ?A \/ ?B |- _ =>
-        idtac "\/-E";
-        destruct H; [tauto1 | tauto1]
     end.
 
 (* 2- *)
@@ -137,10 +136,15 @@ End Tests.
     - [\/ - E]
 La règle [-> - E] n'est pas réversible en effet si on considère le séquent : 
 
-          C |- A                B, C |- C
-    -------------------------------------------
-                   A -> B, C |- C
-    *)
+   C |- A        B, C |- C
+  ------------------------
+       A -> B, C |- C
+
+Le code obtenu est donc un copier coller de tauto1 à un e exception près :
+Toutes stratégies non atomiques (i.e. celles qui ne sont pas [Ax] [False-E] ou 
+[True-I]) à l'exception de [-> - E] sont de la forme 
+  ( <\tactique essayée\> ) + ( idtac "back [rulename]"k; fail 1 )
+*)
 
 Ltac tauto2 :=
     match goal with
@@ -152,11 +156,11 @@ Ltac tauto2 :=
         assumption
     (* Rule : [False - E] *)
     | _ : False |- _ =>
-        idtac "False - E";
+        idtac "False-E";
         contradiction
     (* Rule : [True - I] *)
     | |- True =>
-        idtac "True - I";
+        idtac "True-I";
         auto
 
     (* ===/  Rules for ->  \=== *)
@@ -164,7 +168,7 @@ Ltac tauto2 :=
     (* Rule : [-> - I]*)
     | |- ?A -> ?B =>
         idtac "->-I";
-        try (intro; tauto2); idtac "back"; fail 1
+        (intro; tauto2) + (idtac "back [->-I]"; fail 1)
     (* Rule : [-> - E]*)
     | H : ?A -> ?B |- ?C =>
         idtac "->-E";
@@ -182,40 +186,38 @@ Ltac tauto2 :=
             |
             clear H; tauto2]
 
+    (* ===/  Rules for \/  \=== *)
+    (* ----------------------- *)
+    (* Rule : [\/ - I] *)
+    | |- ?A \/ ?B =>
+        idtac "\/-I";
+        ((left; tauto2) || (right; tauto2)) + (idtac "back [\/-I]"; fail 1)
+    (* Rule : [\/ - E] *)
+    | H: ?A \/ ?B |- _ =>
+        idtac "\/-E";
+        (destruct H; [tauto2 | tauto2]) + (idtac "back [\/-E]"; fail 1)
 
     (* ===/  Rules for /\  \=== *)
     (* ----------------------- *)
     (* Rule : [/\ - I] *)
     | |- ?A /\ ?B =>
         idtac "/\-I";
-        try (split; [tauto2 | tauto2]); idtac "back /\-I"; fail 1 
+        (split; [tauto2 | tauto2]) + (idtac "back [/\-I]"; fail 1) 
     (* Rule : [/\ - E] *)
     | H: ?A /\ ?B |- _ =>
         idtac "/\-E";
-        try (destruct H; tauto2); idtac "back"; fail 1
-
-    (* ===/  Rules for \/  \=== *)
-    (* ----------------------- *)
-    (* Rule : [\/ - I] *)
-    | |- ?A \/ ?B =>
-        idtac "\/-I";
-        try (left; tauto2) || (right; tauto2); idtac "back"; fail 1
-    (* Rule : [\/ - E] *)
-    | H: ?A \/ ?B |- _ =>
-        idtac "\/-E";
-        (destruct H; [tauto2 | tauto2]) + (idtac "back \/-E"; fail 1)
+        (destruct H; tauto2) + (idtac "back [/\-E]"; fail 1)
     end.
 
-
-Lemma dummy_back : forall A B C : Prop,
-    A /\ C -> B \/ B -> B /\ C.
+(* 2- *)
+Lemma exple_backtracking : forall A B  : Prop,
+    (* A /\ A -> A /\ A -> A /\ A -> *) A /\ A -> A /\ A -> (B \/ B) \/ A.
 Proof.
-    intros A B C.
-    tauto1.
+    intros A B.
+    tauto1.  (* Fait un grand nombre d'essais d'appels de règles *)
     Restart.
-    intros A B C.
-    Print nil.
-    tauto2.
+    intros A B.
+    tauto2. (* Fait beaucoup moins de règles... *)
 Qed. 
 
 (* III. Formalizing the tactic *)
@@ -224,9 +226,11 @@ Qed.
 (* III.1. Tactic steps *)
 (* ------------------- *)
 
-Require Import List.
+(* 1- *)
 
-Inductive form : Type :=
+Require Import List Arith.
+
+Inductive form : Set :=
     | Leaf : nat -> form (* Axiom was already taken :( *)
     | True_form : form
     | False_form : form
@@ -236,51 +240,188 @@ Inductive form : Type :=
 
 Print List.
 
-Definition seq : Type :=
+Definition seq : Set :=
     (list form)* form.
 
-Print In.
-Print true.
-Print True.
+
+(* 2- *)
+
+Fixpoint equal_form (f1 f2 : form) : bool :=
+  match f1, f2 with
+  | Leaf n1, Leaf n2 => beq_nat n1 n2
+  | True_form, True_form 
+  | False_form, False_form => true
+  | Impl f1g f1d, Impl f2g f2d => andb (equal_form f1g f2g) (equal_form f1d f2d)
+  | And f1g f1d, Impl f2g f2d => andb (equal_form f1g f2g) (equal_form f1d f2d)
+  | Or f1g f1d, Impl f2g f2d => andb (equal_form f1g f2g) (equal_form f1d f2d) 
+  | _, _ => false
+  end.
+
+
+Fixpoint in_bool (l : list form) (f : form) :=
+  match l with
+  | nil => false
+  | cons f' l' => orb (equal_form f' f) (in_bool l' f)
+  end.
 
 Definition is_leaf ( seq : seq ) : bool :=
     match snd seq with
-    | True_form => true
-    | Leaf n => (
-        match In (Leaf n) (fst seq) with
-        | True => true
-        (* | _ => false *)
-        end
-    )
-    | _ => (
-        match In False_form (fst seq) with
-        | False => false
-        (* | _ => true *)
-        end
-    )
+    | True_form => true                   (* Si on doit prouver le TOP *)
+    | Leaf _ as f =>                      (* Quand le but est une variable *)
+      orb (in_bool (fst seq) f)             (* On regarde si elle n'est pas dans les hypothèses *)
+          (in_bool (fst seq) False_form)    (* Sinon, on regarde si on a l'hypothèse BOTTOM *)
+    | _ => in_bool (fst seq) False_form   (* Pour les autres cas, on regarde si on a l'hypothèse BOTTOM *)
     end.
 
 Print is_leaf.
 
-Print or.
+(* 3- *)
+
+Definition subgoals := list (list seq).
+
+Fixpoint merge_subgoals (l1 : subgoals) (l2 : subgoals) : subgoals :=
+  match l1 with 
+  | nil => l2
+  | hd::tl => hd::(merge_subgoals tl l2)
+  end.
+
+Definition picked_hyp := list (form * list form).
+
+Fixpoint add_hyp (to_add : form) (hyps : picked_hyp) : picked_hyp :=
+  match hyps with
+  | nil => nil 
+  | cons (hyp, other_hyps) tl => 
+    (hyp, cons to_add other_hyps) :: (add_hyp to_add tl)
+  end.
+
+Fixpoint pick_hyp_aux ( hyps : list form ) : picked_hyp :=
+  match hyps with
+  | nil => nil
+  | cons hd tl => 
+   (hd, tl) :: (add_hyp hd (pick_hyp_aux tl))
+  end.
+
+Definition pick_hyp (seq : seq) : picked_hyp :=
+  pick_hyp_aux (fst seq).
+
+Definition intro_rules (seq : seq) : subgoals :=
+  match snd seq with
+  | Leaf _ | True_form | False_form => nil
+  | Impl premisse conclusion =>
+    ((cons premisse (fst seq), conclusion)::nil )::nil
+  | And left_form right_form =>
+    ((fst seq, left_form) :: (fst seq, right_form) ::nil)::nil
+  | Or left_form right_form =>
+    ((fst seq, left_form)::nil) :: ((fst seq, right_form)::nil)::nil
+  end.
+
+Definition elim_rules (hyps : form * list form) (goal : form) : subgoals :=
+  match fst hyps with
+  | Leaf _ | True_form | False_form => nil
+  | Impl premisse conclusion =>
+    ((snd hyps, premisse) :: (conclusion::(snd hyps), goal) :: nil)::nil
+  | And left_form right_form =>
+    ((left_form::right_form::(snd hyps), goal)::nil)::nil
+  | Or left_form right_form =>
+    ( (left_form :: (snd hyps), goal) :: (right_form :: (snd hyps), goal) :: nil )::nil
+  end.
+
+
+  
+Fixpoint elim_rules_multi (hyps : picked_hyp) (goal : form) : subgoals :=
+  match hyps with
+  | nil => nil
+  | hyp :: tl => merge_subgoals (elim_rules hyp goal) (elim_rules_multi tl goal)
+  end.
+
+Definition step (seq : seq) : subgoals :=
+  merge_subgoals (intro_rules seq) (elim_rules_multi (pick_hyp seq) (snd seq)).
+
+(* 4- *)
+
+Fixpoint tauto (fuel : nat) (seqt : seq) : bool :=
+  if is_leaf seqt then
+    true
+  else
+    match fuel with
+    | 0 => false
+    | S n =>
+      (* le nom « étrange » donné à subgoalz corrige les problèmes de typage
+         pour éviter le shadowing du type par la variable *)
+      let subgoalz := step seqt in
+      let fix handle_subgoal (subgoal: list seq) : bool :=
+        match subgoal with
+        | nil => true 
+        | seq :: tl => andb(tauto n seq) (handle_subgoal tl)
+        end in
+      let fix handle_subgoals (subgoalz: subgoals) : bool :=
+        match subgoalz with
+        | nil => false
+        | subgoal :: tl => orb (handle_subgoal subgoal) (handle_subgoals tl)
+        end in
+      handle_subgoals subgoalz
+    end. 
+
+
+(* III.2. Termination *)
+(* ------------------ *)
+
+(* 1- *)
+
+Fixpoint size_form (f : form) : nat :=
+  match f with
+  | Leaf _ | True_form | False_form => 1
+  | Impl f1 f2 => 1 + (size_form f1) + (size_form f2)
+  | And f1 f2 => 1 + (size_form f1) + (size_form f2)
+  | Or f1 f2 => 1 + (size_form f1) + (size_form f2)
+  end.
+
+Fixpoint size_hyps (hyps : list form) : nat :=
+  match hyps with
+  | nil => 0
+  | hyp :: hyps => (size_form hyp) + (size_hyps hyps)
+  end.
+
+Definition size_seq (seq : seq) : nat :=
+  let hyps := fst seq in
+  let goal := snd seq in
+  (size_hyps hyps) + (size_form goal).
+
+(* 2- *)
+
+Lemma size_decrease : 
+  forall s : seq, forall s' : seq, forall subgoal : list seq, 
+  (In subgoal (step s)) /\ (In s' subgoal) -> (size_seq s') < (size_seq s).
+Proof.
+  intros s s' sg.
+  intros H.
+  
+  simpl.
+Admitted.
 
 (* III.3. Soundness *)
 (* ---------------- *)
 
 (* 1- *)
-Fixpoint sem (f : form) : Prop :=
+Fixpoint sem (f : form) (sem_nat : nat -> Prop) : Prop :=
     match f with
     | True_form => True
     | False_form => False
-    | Leaf _ => True (* TODO *)
-    | Impl f1 f2 => (sem f1) -> (sem f2)
-    | And f1 f2 => (sem f1) /\ (sem f2)
-    | Or f1 f2 => (sem f1) \/ (sem f2)
+    | Leaf n => sem_nat n
+    | Impl f1 f2 => (sem f1 sem_nat) -> (sem f2 sem_nat)
+    | And f1 f2 => (sem f1 sem_nat) /\ (sem f2 sem_nat)
+    | Or f1 f2 => (sem f1 sem_nat) \/ (sem f2 sem_nat)
     end.
 
-Require Import Fold_Left_Recursor.
+
+Fixpoint sem_hyps (hyps : list form) (sem_nat : nat -> Prop) :=
+  match hyps with
+  | nil => True 
+  | hyp :: hyps => (sem hyp sem_nat) /\ (sem_hyps hyps sem_nat)
+  end.
 
 Definition seq_valid (s : seq) : Prop :=
-    sem (fst s) -> sem (snd s).
+    forall sem_nat : nat -> Prop, 
+    sem_hyps (fst s) sem_nat -> sem (snd s) sem_nat.
 
 Lemma Leaf_sound : forall s : seq, is_leaf s  -> sem  
